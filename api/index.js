f77a2f7045090ab0b16b2e763d231c408c6d40da
Matt Horton <matt-horton@Matts-MacBook-Pro.local>
2022-11-10 15:23:44 +0000

    Added PORT env variable

--- api/index.js
+++ api/index.js
@@ -3,39 +3,39 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT | 3001;
 const authRoutes = require("./auth/routes");
 
 // Middleware
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
 });
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-    })
+  cors({
+    origin: "http://localhost:3000",
+  })
 );
 app.use(cookieParser());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Hello there");
+  res.send("Hello there");
 });
 
 app.post("/users", (req, res) => {
-    const { name, email } = req.body;
+  const { name, email } = req.body;
 
-    queries.createUser(name, email);
+  queries.createUser(name, email);
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+  console.log(`Listening on port: ${PORT}`);
 });
